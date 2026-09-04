b4fe172ac5830dcb7e573ee23eb1f73fb4aa3223
Celeste Agorreca <celeagorreca@gmail.com>
2026-09-04 20:32:47 -0300

    Update tp1.js

--- tp1/tp1.js
+++ tp1/tp1.js
@@ -11,8 +11,7 @@ let fondo = [];
 
 let estadoP;
 let contadorGeneral;
-let frameCambio = 0;
-let tiempito = 500;
+let velocidad = 10;
 
 function preload(){
 // fondo
@@ -30,61 +29,73 @@ function preload(){
 function setup() {
   createCanvas(800, 600);
   background(200);
+  
   a = d = c = 0;
   estadoP = 0;
   contadorGeneral = 0;
 }
 
 function draw() {
-  image( fondo[0], 0, 0 );
 
-    if (millis() - frameCambio >= tiempito) {
-
-// Pikachu
-  switch (estadoP){
-  case 0:
-    image( pikachuA[a], 270, 600-contadorGeneral*4 );
-  break;
-
-  case 1:
-    image( pikachuD[d % pikachuD.length], 300+contadorGeneral*4, 260 );
-   break;
-
-   case 2:
-     image( pikachuC[c], 600, 260-contadorGeneral*4 );
-   break;
- }   
-    // Red
-    switch (estadoP) {
-  case 0:
-    image( redA[a], 330, 600-contadorGeneral*4 );
-    a++;
-    if ( a > 2 ) a = 0;
-    if( cambiaEstado() ){
-    estado (1);
-}
-    
-  break;
-  
-  case 1:
-    image( redD[d], 320+contadorGeneral*4, 260 );
-    d++;
-    if ( d > 2 ) d = 0;
-    if( cambiaEstado() ){
-    estado (2);
-}
-  break;
-  
-  case 2:
-    image( redC[c], 660, 260-contadorGeneral*4 );
-    c++;
-    if( c > 1 ) c = 0;
-    if( cambiaEstado() ){
-    estado (0);
-} 
-  break;
-} 
-
+  image(fondo[0], 0, 0);
+  if (frameCount % velocidad == 0) {
     contadorGeneral++;
- }
+
+    if (estadoP == 0) {
+      a++;
+      if (a >= redA.length) a = 0;
+      if (cambiaEstado()) {
+        estado(1);
+      }
+    }
+
+    else if (estadoP == 1) {
+      d++;
+      if (d >= redD.length) d = 0;
+      if (cambiaEstado()) {
+        estado(2);
+      }
+    }
+
+    else if (estadoP == 2) {
+      c++;
+      if (c >= redC.length) c = 0;
+      if (cambiaEstado()) {
+        estado(0);
+      }
+    }
+  }
+
+  // Pikachu
+  switch (estadoP) {
+
+    case 0:
+      image(pikachuA[a], 270, 600 - contadorGeneral * 4);
+      break;
+
+    case 1:
+      image(pikachuD[d % pikachuD.length], 300 + contadorGeneral * 4, 260);
+      break;
+
+    case 2:
+      image(pikachuC[c], 600, 260 - contadorGeneral * 4);
+      break;
+  }
+
+
+  // Red
+  switch (estadoP) {
+
+    case 0:
+      image(redA[a], 330, 600 - contadorGeneral * 4);
+      break;
+
+    case 1:
+      image(redD[d], 320 + contadorGeneral * 4, 260);
+      break;
+
+    case 2:
+      image(redC[c], 660, 260 - contadorGeneral * 4);
+      break;
+  }
 }
